615e624597bcf03c6a8b115ac1d7ce50c582f655
Ruwan Ranaweera <rwnranaweera@gmail.com>
2022-06-09 08:56:33 +0530

    react app sample 3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  const hello =10;
+  return (
+    
+    <div className="App">Hello World </div>
+    <div className="App">Hello World 2</div>
+  );
+}
+
+export default App;
